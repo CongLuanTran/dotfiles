23a3a533324e5ef1a091c09ee4bda915a0c9bb75
Cong Luan Tran <congluan.tran19@gmail.com>
2024-12-17 02:41:48 +0700

    Change some minor nvim configuration

--- .config/nvim/lua/plugins/hyprconf.lua
+++ .config/nvim/lua/plugins/hyprconf.lua
@@ -1,25 +1,31 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "hyprlang",
-      "rasi",
-      "jsonc",
-      "bash",
-      "css",
-      "scss",
-    })
-    vim.filetype.add({
-      extension = { rasi = "rasi" },
-      pattern = {
-        [".*/waybar/config"] = "jsonc",
-        [".*/waybar/*.css"] = "scss",
-        [".*/kitty/*.conf"] = "bash",
-        [".*/hypr/.*%.conf"] = "hyprlang",
-      },
-    })
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "hyprlang",
+        "rasi",
+        "jsonc",
+        "bash",
+        "css",
+        "scss",
+      })
+      vim.filetype.add({
+        extension = { rasi = "rasi" },
+        pattern = {
+          [".*/waybar/config"] = "jsonc",
+          [".*/waybar/*.css"] = "scss",
+          [".*/hypr/.*%.conf"] = "hyprlang",
+        },
+      })
 
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  {
+    "fladson/vim-kitty",
+    ft = "kitty",
+    tag = "v1.1", -- You can select a tagged version
+  },
 }
